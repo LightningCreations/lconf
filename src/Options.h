e96c762c182236e7c1eb623b8a0c8682cf7ba589
Ray Redondo <rdrpenguin04@gmail.com>
2020-06-14 20:22:18 -0500

    And yet more formatting fixes

--- src/Options.h
+++ src/Options.h
@@ -8,27 +8,27 @@
 #include <lstate.h>
 #include <Map.h>
 
-struct Options{
-    const char* prefix;
-    const char* target;
-    const char* module_path;
-    const char* generator;
-    const char* srcdir;
-    const char* builddir;
-    TreeMap* with;
-    TreeMap* enable;
+struct Options {
+    const char *prefix;
+    const char *target;
+    const char *module_path;
+    const char *generator;
+    const char *srcdir;
+    const char *builddir;
+    TreeMap *with;
+    TreeMap *enable;
 };
 
-struct Options* loadoptions(int argc,char** argv);
+struct Options* loadoptions(int argc, char **argv);
 
 void freeoptions(struct Options*);
 
-void load_optlib(lua_State* L,struct Options*);
+void load_optlib(lua_State *L, struct Options*);
 
-int opt_enable(lua_State* L);
-int opt_with(lua_State* L);
-int opt_generator(lua_State* L);
-int opt_target(lua_State* L);
-int opt_host(lua_State* L);
+int opt_enable(lua_State *L);
+int opt_with(lua_State *L);
+int opt_generator(lua_State *L);
+int opt_target(lua_State *L);
+int opt_host(lua_State *L);
 
 #endif //LCONF_OPTIONS_H
