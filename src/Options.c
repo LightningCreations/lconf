07b66a02561eac334446aaa9ede78eb1f030f831
Ray Redondo <rdrpenguin04@gmail.com>
2020-06-14 20:07:44 -0500

    Clean up formatting

--- src/Options.c
+++ src/Options.c
@@ -9,15 +9,15 @@
 #include <Configure.h>
 
 bool strless(const void* k1,const void* k2){
-    return strcmp((const char*)k1,(const char*)k2)<0;
+    return strcmp((const char*)k1, (const char*)k2) < 0;
 }
 
 void print_help();
 
-struct Options* loadoptions(int argc,char** argv){
+struct Options* loadoptions(int argc, char** argv){
     struct Options* ret = malloc(sizeof(struct Options));
-    ret->with = map_new(NULL,strless,NULL,NULL);
-    ret->enable = map_new(NULL,strless,NULL,NULL);
+    ret->with = map_new(NULL, strless, NULL, NULL);
+    ret->enable = map_new(NULL, strless, NULL, NULL);
     ret->prefix = LCONF_DEFAULT_PREFIX;
     ret->module_path = LCONF_MODULE_PATH;
     ret->generator = LCONF_DEFAULT_GENERATOR;
@@ -25,11 +25,11 @@ struct Options* loadoptions(int argc,char** argv){
     ret->target = "host";
     for(int i = 0;i<argc;i++){
         char* arg = argv[i];
-        if(*arg=='-'&&*++arg=='-'){
+        if((*arg == '-') && (*++arg == '-')){
             arg++;
             char* begin = strtok(arg,"=");
-            if(strcmp(begin,"prefix")==0){
-                ret->prefix = strtok(NULL,"");
+            if(strcmp(begin, "prefix") == 0){
+                ret->prefix = strtok(NULL, "");
 		if(!ret->prefix){
                     print_help();
 		    exit(1);
